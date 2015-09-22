bcf19e834200809b29c908aadf6ff3f923e7e273
Patrick Gerken <patrick.gerken@zumtobelgroup.com>
2015-09-22 14:29:04 +0200

    Make newest code analysis happy

--- src/collective/normalize_buildout/tests/test_script.py
+++ src/collective/normalize_buildout/tests/test_script.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
-from StringIO import StringIO
 from collective.normalize_buildout.cmd import sort
 from collective.normalize_buildout.testing import BaseTestCase
+from StringIO import StringIO
 
 
 class TestScript(BaseTestCase):
